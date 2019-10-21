570f1f9f245460fca12ff1626e75aa2768dfff78
Strokes-lla <981703430@qq.com>
2019-10-21 15:47:00 +0800

    日常提交

--- src/view/Proxy.vue
+++ src/view/Proxy.vue
@@ -1,44 +1,45 @@
 <template>
-    <div class="box_warpper">
-    </div>
+  <div class="box_warpper">
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {}
+  export default {
+    data() {
+      return {}
+    },
+    filters: {},
+    methods: {},
+    components: {},
+    mounted() {
+      let obj = {
+        name: 'yy',
+        sex: "n",
+        // age:18,
+      };
+      Reflect.deleteProperty(obj, 'name');
+      console.log(obj);
+
+
+      let proxy = new Proxy(obj, {
+        get(target, property) {
+          // console.log(property);
+          if (property == 'age') {
+            return 35;
+          } else {
+            return target[property]
+          }
         },
-        filters: {},
-        methods: {},
-        components: {},
-        mounted() {
-          let obj={
-            name:'yy',
-            sex:"n",
-            // age:18
-          };
-          Reflect.deleteProperty(obj,'name');
-          console.log(obj);
+      });
+      // console.log(proxy.name);
+      // console.log(proxy.age);
 
 
-          let proxy = new Proxy(obj, {
-            get: function(target, property) {
-              console.log(property);
-              if(property=='age'){
-                return 35;
-              }else{
-                return target[property]
-              }
-            }
-          });
-          console.log(proxy.name);
-          console.log(proxy.age);
-
-        }
     }
+  }
 </script>
 
 <style lang="less" scoped>
-    .box_warpper {
-    }
+  .box_warpper {
+  }
 </style>
---
+++ src/view/Reflect.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box_warpper">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        filters: {},
+        methods: {},
+        components: {},
+        mounted() {
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    .box_warpper {
+    }
+</style>
