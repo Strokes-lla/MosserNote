f583e697cafc41b386ad04a74540a668ad455d50
Jarvis-specter <981703430@qq.com>
2019-12-24 22:42:11 +0800

    Specter test

--- src/view/highcharts.vue
+++ src/view/highcharts.vue
@@ -5,92 +5,163 @@
 </template>
 
 <script>
-  import Highcharts from 'highcharts'
+    import Highcharts from 'highcharts'
 
-  export default {
-    data() {
-      return {}
-    },
-    filters: {},
-    methods: {
-      init() {
-        Highcharts.chart('container', {
-          xAxis: {
-            min: -0.5,
-            max: 5.5
-          },
-          yAxis: {
-            min: 0,
-            plotLines: [{
-              color: 'black',
-              dashStyle: 'dot',
-              width: 2,
-              value: 3,
-              label: {
-                align: 'right',
-                style: {
-                  fontStyle: 'italic'
-                },
-                text: '正常糖的摄入量 50g/天',
-                x: 2
-              },
-              zIndex: 3
-            },{
-              color: 'black',
-              dashStyle: 'dot',
-              width: 2,
-              value: 5,
-              label: {
-                align: 'right',
-                style: {
-                  fontStyle: 'italic'
-                },
-                text: '正常糖的摄入量 50g/天',
-                x: 2
-              },
-              zIndex: 3
-            }]
-          },
-          title: {
-            text: '包含趋势线的散点图'
-          },
-          series: [{
-            type: 'line',
-            name: '回归线',
-            data: [[0, 1.11], [5, 4.51]],
-            marker: {
-              enabled: false
-            },
-            states: {
-              hover: {
-                lineWidth: 0
-              }
-            },
-            enableMouseTracking: false
-          }, {
-            type: 'scatter',
-            name: '观测值',
-            data: [1, 5],
-            marker: {
-              radius: 4
+    export default {
+        data() {
+            return {}
+        },
+        filters: {},
+        methods: {
+            init() {
+                Highcharts.chart('container', {
+                    xAxis: {
+                        min: -0.5,
+                        max: 5.5
+                    },
+                    yAxis: {
+                        min: -3,
+                        max: 23,
+                        plotLines: [{
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: 12,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '+1倍标准差（+1a）',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }, {
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: 6,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '-1倍标准差（+1a）',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }, {
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: 18,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '+2倍标准差（+2a）',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }, {
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: 2,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '-2倍标准差（-2a）',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }, {
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: 22,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '+3倍标准差（+3a）',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }, {
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: -3,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '-3倍标准差（-3a）',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }, {
+                            color: 'black',
+                            dashStyle: 'dot',
+                            width: 2,
+                            value: 12,
+                            label: {
+                                align: 'right',
+                                style: {
+                                    fontStyle: 'italic'
+                                },
+                                text: '平均值',
+                                x: 2
+                            },
+                            zIndex: 3
+                        }]
+                    },
+                    title: {
+                        text: ''
+                    },
+                    series: [{
+                        type: 'line',
+                        name: '',
+                        data: [[0, 1.11], [3, 4.51], [5, 4.51], [7, 2.1]],
+                        marker: {
+                            enabled: false
+                        },
+                        states: {
+                            hover: {
+                                lineWidth: 0
+                            }
+                        },
+                        enableMouseTracking: false
+                    }, {
+                        type: 'scatter',
+                        name: '',
+                        data: [1, 3, 4, 5, 6, 7, 8, 9],
+                        marker: {
+                            radius: 4
+                        }
+                    }]
+                });
             }
-          }]
-        });
-      }
-    },
-    components: {},
-    mounted() {
-      this.init()
+        },
+        components: {},
+        mounted() {
+            this.init()
+        }
     }
-  }
 </script>
 
 <style lang="less" scoped>
   .box_warpper {
-    #container{
-      margin-top:30px;
-      width:800px;
-      height:500px;
+    #container {
+      margin-top: 30px;
+      width: 800px;
+      height: 500px;
     }
   }
 </style>
